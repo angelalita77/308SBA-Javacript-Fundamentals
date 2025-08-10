d9528b52c647f585b96a3c5df23a929db493d427
angelalita77 <anjeanette@gmail.com>
2025-08-10 14:07:38 -0400

    Testing out how to use try and catch

--- script.js
+++ script.js
@@ -78,20 +78,34 @@ const LearnerSubmissions = [
 
 function getLearnerData(course, ag, submissions) {
   // here, we would process this data to achieve the desired result.
-  const result = [
-    {
-      id: 125,
-      avg: 0.985, // (47 + 150) / (50 + 150)
-      1: 0.94, // 47 / 50
-      2: 1.0 // 150 / 150
-    },
-    {
-      id: 132,
-      avg: 0.82, // (39 + 125) / (50 + 150)
-      1: 0.78, // 39 / 50
-      2: 0.833 // late: (140 - 15) / 150
-    }
-  ];
+
+  let result = [];
+
+
+
+
+
+  // Our code here
+  console.log(`Compare`, course);
+  console.log(`Assignment Group`, ag);
+  console.log('Submissions', submissions);
+
+
+
+  //   const result = [
+  //     {
+  //       id: 125,
+  //       avg: 0.985, // (47 + 150) / (50 + 150)
+  //       1: 0.94, // 47 / 50
+  //       2: 1.0 // 150 / 150
+  //     },
+  //     {
+  //       id: 132,
+  //       avg: 0.82, // (39 + 125) / (50 + 150)
+  //       1: 0.78, // 39 / 50
+  //       2: 0.833 // late: (140 - 15) / 150
+  //     }
+  //   ];
 
   return result;
 }
@@ -99,3 +113,16 @@ function getLearnerData(course, ag, submissions) {
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
 console.log(result);
+
+function testErr() {
+  try {
+    const name = "Dave";
+    name = "Joe";
+  } catch (err) {
+    console.error("Whoops! Looks like somthing happened:")
+    console.error(err);
+  }
+}
+
+testErr();
+
